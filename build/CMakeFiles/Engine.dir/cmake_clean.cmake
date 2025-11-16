3f28e907cfb6e857ab43942057174904073a00d7
TravisBoyd884 <travisboyd741@gmail.com>
2025-11-16 15:11:48 -0500

    Initial Commit

---
+++ build/CMakeFiles/Engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine.dir/src/glad.c.o"
+  "CMakeFiles/Engine.dir/src/glad.c.o.d"
+  "CMakeFiles/Engine.dir/src/main.cpp.o"
+  "CMakeFiles/Engine.dir/src/main.cpp.o.d"
+  "Engine"
+  "Engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
