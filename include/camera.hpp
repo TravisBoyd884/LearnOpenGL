83d15332323cdd1f3dd21dcec2068cdc246a439e
TravisBoyd884 <travisboyd741@gmail.com>
2025-11-22 16:55:57 -0500

    Saved verties

--- include/camera.hpp
+++ include/camera.hpp
@@ -68,9 +68,11 @@ public:
   void ProcessKeyboard(Camera_Movement direction, float deltaTime) {
     float velocity = MovementSpeed * deltaTime;
     if (direction == FORWARD)
-      Position += Front * velocity;
+      // Restricts y position to 0
+      Position += glm::vec3(Front.x, 0.0f, Front.z) * velocity;
     if (direction == BACKWARD)
-      Position -= Front * velocity;
+      // Restricts y position to 0
+      Position -= glm::vec3(Front.x, 0.0f, Front.z) * velocity;
     if (direction == LEFT)
       Position -= Right * velocity;
     if (direction == RIGHT)
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+
 GLFWwindow *create_window();
 int init_glad();
 
@@ -30,15 +31,13 @@ float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
 // timing
-float deltaTime = 0.0f; // time between current frame and last frame
+float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 int main() {
   GLFWwindow *window = create_window();
-  if (window == NULL)
-    return -1;
-  if (init_glad() == -1)
-    return -1;
+
+  init_glad();
 
   glfwSetCursorPosCallback(window, mouse_callback);
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -46,34 +45,49 @@ int main() {
 
   Shader myShader("../shaders/shader.vert", "../shaders/shader.frag");
 
-  float vertices[] = {
-      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,
-      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
-      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+  // fmt:off
+  float vertices[] = {-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //
+                      0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, //
+                      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
+                      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
+                      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, //
+                      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, //
 
-      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
-      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-      -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,
+                      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
+                      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
+                      0.5f,  -0.5f, 0.5f,  1.0f, 1.0f, //
+                      0.5f,  -0.5f, 0.5f,  1.0f, 1.0f, //
+                      -0.5f, -0.5f, 0.5f,  0.0f, 1.0f, //
+                      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
 
-      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,
+                      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, //
+                      -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f, //
+                      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+                      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+                      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
+                      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, //
 
-      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
-      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,
-      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+                      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, //
+                      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, //
+                      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, //
+                      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, //
+                      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, //
+                      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, //
 
-      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,
-      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
-      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+                      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
+                      0.5f,  -0.5f, -0.5f, 1.0f, 1.0f, //
+                      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
+                      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, //
+                      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, //
+                      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, //
 
-      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
-      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
-  // unsigned int indices[] = {
-  //     0, 1, 3, // first triangle
-  //     1, 2, 3  // second triangle
-  // };
+                      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f,  //
+                      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,  //
+                      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  //
+                      0.5f,  0.5f,  0.5f,  1.0f, 0.0f,  //
+                      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f,  //
+                      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f}; //
+  // fmt:on
   unsigned int VBO, VAO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
@@ -83,10 +97,6 @@ int main() {
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-  //              GL_STATIC_DRAW);
 
   // position attribute
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
@@ -166,12 +176,6 @@ int main() {
     // render container
     myShader.use();
 
-    // model matrix
-    // const float radius = 10.0f;
-    // float camX = sin(glfwGetTime()) * radius;
-    // float camZ = cos(glfwGetTime()) * radius;
-    // pass projection matrix to shader (note that in this case it could change
-    // every frame)
     glm::mat4 projection =
         glm::perspective(glm::radians(45.0f),
                          (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -180,8 +184,6 @@ int main() {
     // camera/view transformation
     glm::mat4 view = camera.GetViewMatrix();
     myShader.setMat4("view", view);
-    // myShader.setMat4("view", view);
-    // myShader.setMat4("projection", projection);
 
     glBindVertexArray(VAO);
     for (unsigned int i = 0; i < 10; i++) {
@@ -200,7 +202,6 @@ int main() {
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
-  // glDeleteBuffers(1, &EBO);
 
   glfwTerminate();
   return 0;
@@ -244,7 +245,7 @@ GLFWwindow *create_window() {
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return window;
+    std::exit(-1);
   }
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
@@ -256,7 +257,7 @@ int init_glad() {
   // glad: load all OpenGL function pointers
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
+    std::exit(-1);
   }
   return 0;
 }
